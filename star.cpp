655a0c76979fcd412909a23bf9ffafb080006f0a
Kerplunk9 <108015762+Kerplunk9@users.noreply.github.com>
2022-06-22 18:05:13 +0200

    Update star.cpp

--- star.cpp
+++ star.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
     }
     int monChoix = atoi(argv[1]);
 
-    // boucle 1
+    //boucle 1
     for (int i = 1; i <= monChoix; i++)
     {
         for (int j = 1; j <= (monChoix * 2 + 1) + ((monChoix * 2) / 2) - i; j++) {
@@ -27,13 +27,13 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }
 
-    // boucle 2a
+    //boucle 2a
     for (int i = 0; i < monChoix * 2 + 1; i++)
     {
         cout << "*";
     }
 
-    // boucle 2b
+    //boucle 2b
     for (int i = monChoix * 2 + 1; i < (2 * (monChoix * 2 + 1)) + (monChoix * 2 - 1); i++)
     {
         if (i < (monChoix * 2 + 1) + (monChoix * 2 - 1))
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
     }
     cout << endl;
 
-    // boucle 3a
+    //boucle 3a
     for (int i = monChoix; i > 0; i--)
     {
         for (int j = 1; j <= monChoix - i; j++) {
@@ -65,7 +65,7 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }
 
-    // boucle 3b
+    //boucle 3b
 
     for (int i = 2; i <= monChoix; i++)
     {
@@ -88,13 +88,13 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }
 
-    // boucle 4a
+    //boucle 4a
     for (int i = 0; i < monChoix * 2 + 1; i++)
     {
         cout << "*";
     }
 
-    // boucle 4b
+    //boucle 4b
     for (int i = monChoix * 2 + 1; i < (2 * (monChoix * 2 + 1)) + (monChoix * 2 - 1); i++)
     {
         if (i < (monChoix * 2 + 1) + (monChoix * 2 - 1))
@@ -104,7 +104,7 @@ int main(int argc, char* argv[]) {
     }
     cout << endl;
 
-    // boucle 5
+    //boucle 5
     for (int i = monChoix; i > 0; i--)
     {
         for (int j = 1; j <= (monChoix * 2 + 1) + ((monChoix * 2) / 2) - i; j++) {
